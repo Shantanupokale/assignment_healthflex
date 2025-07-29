ead3ca936d2c251d5b646568993881ae5774bb33
Shantanupokale <shantanupokale51@gmail.com>
2025-07-29 16:14:19 +0530

    feat: added Pages And Routes using React router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 import './App.css'
+import Home from './pages/Home';
+import CreateTimer from './pages/CreateTimer';
+import History from './pages/History';
+
 function App() {
 
 
   return (
     <>
-      <div className='bg-red-500 h-50 '>
-        saas 
-        
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/create' element={<CreateTimer/>}/>
+          <Route path='/history' element={<History/>}/>
+        </Routes>
+    </Router>
      
     </>
   )
---
+++ src/pages/CreateTimer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateTimer = () => {
+  return (
+    <div>CreateTimer</div>
+  )
+}
+
+export default CreateTimer

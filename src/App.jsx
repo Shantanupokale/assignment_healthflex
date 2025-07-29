0443001163045ae982f11de50084e5e382cae20f
Shantanupokale <shantanupokale51@gmail.com>
2025-07-29 16:40:15 +0530

    feat: added theme provider for light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-import './App.css'
-import Home from './pages/Home';
-import CreateTimer from './pages/CreateTimer';
-import History from './pages/History';
+import "./App.css";
+import Home from "./pages/Home";
+import CreateTimer from "./pages/CreateTimer";
+import History from "./pages/History";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-
-
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/create' element={<CreateTimer/>}/>
-          <Route path='/history' element={<History/>}/>
-        </Routes>
-    </Router>
-     
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateTimer />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
-export const ThemeProvider = () => {
+const ThemeProviderContext = createContext({
+  theme: 'light',
+  setTheme: () => null,
+});
+
+export function ThemeProvider({ children, storageKey = 'timer-app-theme', ...props }) {
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem(storageKey) || 'light';
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove('light', 'dark');
+    root.classList.add(theme);
+  }, [theme]);
+
+  const value = {
+    theme,
+    setTheme: (newTheme) => {
+      localStorage.setItem(storageKey, newTheme);
+      setTheme(newTheme);
+    },
+  };
+
   return (
-    <div>ThemeProvider</div>
-  )
+    <ThemeProviderContext.Provider value={value} {...props}>
+      {children}
+    </ThemeProviderContext.Provider>
+  );
 }
+
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext);
+
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+
+  return context;
+};
